1540b2ebea51560c17523d588446e7e37a3bb940
John Ellison <johnjellison@gmail.com>
2023-01-10 20:31:44 -0600

    removed filetool project and created interface for it

--- FileSorterCLILib/Models/MyFileInfo.cs
+++ FileSorterCLILib/Models/MyFileInfo.cs
@@ -2,8 +2,6 @@ namespace FileSorterCLILib.Models;
 
 public class MyFileInfo
 {
-    public string Directory { get; set; }
     public string DirectoryName { get; set; }
     public string Extension { get; set; }
-    public string Name { get; set; }
 }
--- FileSorterCLILib/SortProcessor.cs
+++ FileSorterCLILib/SortProcessor.cs
@@ -4,6 +4,7 @@ namespace FileSorterCLILib;
 
 public class SortProcessor : IProcessor
 {
+    // TODO need to create the nuget package in order to do it this way.  
     private readonly IFileTool _fileTool;
     private static string CanHandle => "Sort";
 
@@ -24,17 +25,26 @@ public class SortProcessor : IProcessor
         // TODO v2 look into returning a dictionary and looping through each key, use benchmark nuget pkg to determine which is best way
         foreach (var fileInfo in fileInfos)
         {
-            var targetDirectory = BuildFileExtensionDirectory(directory, fileInfo);
-            if (!_fileTool.DirectoryExists(targetDirectory)) _fileTool.CreateDirectory(targetDirectory);
+            var targetDirectory = BuildTargetDirectory(directory, fileInfo);
+            if (!_fileTool.DirectoryExists(targetDirectory))
+                _fileTool.CreateDirectory(targetDirectory);
         
             _fileTool.MoveFile(fileInfo.DirectoryName, targetDirectory);
         }
     }
 
-    private static string BuildFileExtensionDirectory(string directory, MyFileInfo fileInfo)
+    private static string BuildTargetDirectory(string directory, MyFileInfo fileInfo)
     {
         var extension = fileInfo.Extension.ToUpper();
         var targetDir = $@"{directory}\{extension}";
         return targetDir;
     }
+}
+
+public interface IFileTool
+{
+    bool DirectoryExists(string targetDirectory);
+    void MoveFile(string directoryName, string targetDirectory);
+    IEnumerable<MyFileInfo> GetFileInfos(string directory);
+    void CreateDirectory(string targetDirectory);
 }
